5715c8ab8586425a7ca5289d41a80068502b5cb0
JKSalmon <pike@mtco.com>
2015-02-07 10:55:38 -0600

    Clean up AutonomousCommand

--- RecycleRushRobot/src/Commands/AutonomousCommand.h
+++ RecycleRushRobot/src/Commands/AutonomousCommand.h
@@ -7,11 +7,6 @@
 class AutonomousCommand: public CommandGroup {
 public:
 	AutonomousCommand();
-	virtual void Initialize();
-	virtual void Execute();
-	virtual bool IsFinished();
-	virtual void End();
-	virtual void Interrupted();
 };
 
 #endif
