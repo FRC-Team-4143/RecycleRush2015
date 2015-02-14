312acda0a7a4164188d8c46176387e1534a8465f
Cade Neumann <cade.s.neumann@students.mths.us>
2015-02-14 09:28:37 -0600

    changed values for autonomous

--- RecycleRushRobot/src/Commands/AutonomousCommand.cpp
+++ RecycleRushRobot/src/Commands/AutonomousCommand.cpp
@@ -6,7 +6,8 @@
 AutonomousCommand::AutonomousCommand() {
 	// TODO - Use AddSequential and AddParallel here
 	AddSequential (new Sleep(SmartDashboard::GetNumber("AutoSleep", 3)));
-	AddSequential (new Tote3UpTimed (0.4));
-	AddSequential (new Drive (-0.3, 0, 0, true, 1.2));
+	AddSequential (new Tote3UpTimed (3));
+	AddSequential (new Drive (-0.5, 0, 0, true, 3));
 	AddSequential (new RotateBy ("RotateBy90", 90));
+	AddSequential (new Drive (0.3, 0, 0, true, 1.5));
 }
