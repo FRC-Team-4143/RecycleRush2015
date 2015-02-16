97b3ceee370cb15072701a02a7033d30784985e8
JKSalmon <pike@mtco.com>
2015-02-15 18:03:39 -0600

    Fixed problem in VictorWrapper with checking PDP channel.

--- RecycleRushRobot/src/Modules/VictorWrapper.h
+++ RecycleRushRobot/src/Modules/VictorWrapper.h
@@ -14,7 +14,9 @@
 
 class VictorWrapper : public Victor {
 public:
-	VictorWrapper(uint32_t channel, bool invertSpeed = false, float scaleSpeed = 1.0, float maxSpeed = 1.0, uint8_t pdpChannel = -1, double maxCurrent = 10.0);
+	static const uint8_t INVALID_PDP_CHANNEL = static_cast<uint8_t>(-1);
+
+	VictorWrapper(uint32_t channel, bool invertSpeed = false, float scaleSpeed = 1.0, float maxSpeed = 1.0, uint8_t pdpChannel = INVALID_PDP_CHANNEL, double maxCurrent = 10.0);
 
 	virtual void Set(float speed, uint8_t syncGroup = 0);
 
