74b550b5b0eeaa2e9cd414000eabde17fab9c492
Cade Neumann <cade.s.neumann@students.mths.us>
2015-02-17 19:21:28 -0600

    changed values for autonomous options

--- RecycleRushRobot/src/Commands/AutoBackup.cpp
+++ RecycleRushRobot/src/Commands/AutoBackup.cpp
@@ -4,5 +4,6 @@
 AutoBackup::AutoBackup() {
 	// TODO - Use AddSequential and AddParallel here
 	AddSequential (new Sleep(SmartDashboard::GetNumber("AutoSleep", 1)));
-	AddSequential (new Drive (-0.5, 0, 0, true, 3));
+	AddSequential (new Drive (-0.5, 0, 0, true, 4));
+	AddSequential (new RotateBy ("RotateBy90", 90));
 }
--- RecycleRushRobot/src/Commands/AutoToteMove.cpp
+++ RecycleRushRobot/src/Commands/AutoToteMove.cpp
@@ -6,11 +6,10 @@
 #include "Tote3DownTimed.h"
 AutoToteMove::AutoToteMove() {
 	// TODO - Use AddSequential and AddParallel here
-	AddSequential (new Sleep(SmartDashboard::GetNumber("AutoSleep", 3)));
+	AddSequential (new Sleep(SmartDashboard::GetNumber("AutoSleep", 1)));
 	AddSequential (new Tote3UpTimed (5));
 	AddSequential (new Drive (-0.5, 0, 0, true, 3));
 	AddSequential (new RotateBy ("RotateBy90", 90));
 	AddSequential (new Drive (0.3, 0, 0, true, 1.5));
 	AddSequential (new Tote3DownTimed (5));
-	//^^make sure this goes down (movedown in cpp?)
 }
